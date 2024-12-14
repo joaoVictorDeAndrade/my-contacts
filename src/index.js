0a842dac5032e556f664d89e66083339f43f0ee4
João Victor Pereira de Andrade <jv.andrade02@gmail.com>
2024-12-14 20:26:54 -0300

    feat: set more cors headers

--- src/index.js
+++ src/index.js
@@ -10,11 +10,6 @@ const app = express();
 app.use(express.json()); // Faz o bodyParser
 app.use(cors);
 app.use(routes);
-
-// Error Handler:
-// - Captura qualquer erro da aplicação
-// - Precisa vir depois da definição das rotas
-// - Não sabe lidar com funções async então precisa da lib express-async-errors
 app.use(errorHandler);
 
 app.listen(3001, () => console.log("🔥 Server running at http://localhost:3001"));
